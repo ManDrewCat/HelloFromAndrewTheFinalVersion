add221683a81f3769bdb2e26cf79055227aa0982
Dobbs <stevedobbs2@byui.edu>
2021-05-12 18:10:15 -0400

    Updated

--- src/pehrson/Main.java
+++ src/pehrson/Main.java
@@ -3,6 +3,8 @@ package pehrson;
 public class Main {
 
     public static void main(String[] args) {
-	System.out.print("Hello from Andrew");
+
+        System.out.println("Hello from Andrew");
+        System.out.println("Hello Andrew, my name is Steve.");
     }
 }
